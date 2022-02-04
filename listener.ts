4afca557d4a51d3c4e76c1c2f029769cd5f86b9b
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-04 11:37:40 +0900

    ok

--- listener.ts
+++ listener.ts
@@ -14,20 +14,13 @@ interface Listener {
 }
 
 export const listeners: Listener[] = [
-  // {
-  //   pattern: new URLPattern({ pathname: "/" }),
-  //   handler: ({ request, url, pattern }) => {
-  //     console.log(request, url, pattern.pathname);
-  //     return new Response(
-  //       'Hello World from <a href="https://github.com/ayame113/deno_deploy_template">ayame113/deno_deploy_template</a> !',
-  //       {
-  //         headers: {
-  //           "Content-Type": "text/html; charset=utf-8",
-  //         },
-  //       },
-  //     );
-  //   },
-  // },
+  {
+    pattern: new URLPattern({ pathname: "/" }),
+    handler: ({ request, url, pattern }) => {
+      console.log(request, url, pattern.pathname);
+      return fetch(new URL("./static/index.html", import.meta.url));
+    },
+  },
   /*{
     // Process the request when this pattern is matched.
     // See also URLPattern API:
